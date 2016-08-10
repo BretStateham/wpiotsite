75eea49dc1fea7d98f3ac9d16400dce6672f64ca
Bret Stateham <Bret.Stateham@microsoft.com>
2016-08-10 13:39:29 -0700

    Testing dashboard news hide sample

--- wp-content/plugins/basic-plugin/basic-plugin.php
+++ wp-content/plugins/basic-plugin/basic-plugin.php
@@ -8,3 +8,13 @@
 * Version: 0.0.1
 * License: GPLv2
 */
+
+function dwwp_remove_dashboard_widget() {
+    //do something
+    remove_meta_box('dashboard_primary', 'dashboard','post_container_1');
+}
+
+//add_action($hook,$function_to_add,$priority,$accepted_args);
+//add_filter();
+
+add_action('wp_dashboard_setup','dwwp_remove_dashboard_widget');
