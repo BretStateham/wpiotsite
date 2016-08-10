6f1f15ad1e2e706087cdf51ce535f5cd0a72c96f
Bret Stateham <Bret.Stateham@microsoft.com>
2016-08-10 13:27:36 -0700

    added description

--- wp-content/plugins/basic-plugin/basic-plugin.php
+++ wp-content/plugins/basic-plugin/basic-plugin.php
@@ -2,6 +2,7 @@
 /**
 * Plugin Name: Basic Plugin
 * Plugin URI: http://bretstateham.com 
+* Description: Basic plug created using the Develop With WP tutorial videos
 * Author: Bret Stateham
 * Author URI: http://bretstateham.com 
 * Version: 0.0.1
