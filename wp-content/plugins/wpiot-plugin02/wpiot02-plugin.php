693883aa6e734f89eb38987838e6a02688fe6e1e
Bret Stateham <Bret.Stateham@microsoft.com>
2016-08-11 15:39:22 -0700

    removed erroneous curl command

--- wp-content/plugins/wpiot-plugin02/wpiot02-plugin.php
+++ wp-content/plugins/wpiot-plugin02/wpiot02-plugin.php
@@ -14,8 +14,6 @@ function wpiot02_send_iot_message_on_comment_post($comment_id, $comment_approved
     require __DIR__ . '/vendor/autoload.php';
 
     try {
-      curl_setopt($ch, CURLOPT_CAINFO, getenv('ProgramFiles(x86)') . '\Git\usr\ssl\certs\ca-bundle.crt');
-
       $connectionString = 'HostName=wpiothub.azure-devices.net;DeviceId=wpiotsite;SharedAccessKey=SdEzHCKOGLTnVvur7gs0VAgC7wsVsYjzWNlie3Urtg0=';
       $client = new AzureIoTHub\DeviceClient($connectionString);
 
