7a768dd8142913bd31a0581dc953ab05a0991998
Bret Stateham <Bret.Stateham@microsoft.com>
2016-08-10 17:10:26 -0700

    removed curl option

--- wp-content/plugins/wpiot-plugin/wpiot-plugin.php
+++ wp-content/plugins/wpiot-plugin/wpiot-plugin.php
@@ -14,7 +14,7 @@ function wpiot_send_iot_message_on_comment_post() {
 
     require __DIR__ . '/vendor/autoload.php';
 
-    curl_setopt($ch, CURLOPT_CAINFO, getenv('ProgramFiles(x86)') . '\Git\usr\ssl\certs\ca-bundle.crt');
+    //curl_setopt($ch, CURLOPT_CAINFO, getenv('ProgramFiles(x86)') . '\Git\usr\ssl\certs\ca-bundle.crt');
 
     $connectionString = 'HostName=wpiothub.azure-devices.net;DeviceId=wpiotsite;SharedAccessKey=SdEzHCKOGLTnVvur7gs0VAgC7wsVsYjzWNlie3Urtg0=';
     $client = new AzureIoTHub\DeviceClient($connectionString);
