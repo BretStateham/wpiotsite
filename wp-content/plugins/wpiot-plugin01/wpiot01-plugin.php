71937a2532514a420f9ed99732579ec679e534c9
Bret Stateham <Bret.Stateham@microsoft.com>
2016-08-11 15:18:35 -0700

    Split Plugins

--- wp-content/plugins/wpiot-plugin01/wpiot01-plugin.php
+++ wp-content/plugins/wpiot-plugin01/wpiot01-plugin.php
@@ -1,26 +1,19 @@
 <?php
 /**
-* Plugin Name: Bret's WordPress Azure IoT Plugin
+* Plugin Name: Bret's WordPress Azure IoT Plugin 01
 * Plugin URI: http://bretstateham.com 
-* Description: Demo plug in to post to an Azure IoT Hub when a comment is posted 
+* Description: Uses Guzzle to post to an Azure Function URL 
 * Author: Bret Stateham
 * Author URI: http://bretstateham.com 
 * Version: 0.0.1
 * License: GPLv2
 */
 
-function wpiot_send_iot_message_on_comment_post($comment_id, $comment_approved, $comment_data) {
+function wpiot01_send_iot_message_on_comment_post($comment_id, $comment_approved, $comment_data) {
     //do something
 
     require __DIR__ . '/vendor/autoload.php';
 
-    //curl_setopt($ch, CURLOPT_CAINFO, getenv('ProgramFiles(x86)') . '\Git\usr\ssl\certs\ca-bundle.crt');
-
-    //$connectionString = 'HostName=wpiothub.azure-devices.net;DeviceId=wpiotsite;SharedAccessKey=SdEzHCKOGLTnVvur7gs0VAgC7wsVsYjzWNlie3Urtg0=';
-    //$client = new AzureIoTHub\DeviceClient($connectionString);
-
-    //$response = $client->sendEvent('Comment Posted!' . $comment_data);
- 
     try {
 
       //Try posting to Azure Function using Guzzle
