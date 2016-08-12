efd554ce9f700a0618572ba36eede61cc43bd1f4
Bret Stateham <Bret.Stateham@microsoft.com>
2016-08-11 18:23:45 -0700

    Cleaned up comments.

--- wp-content/plugins/wpiot-plugin/wpiot-plugin.php
+++ wp-content/plugins/wpiot-plugin/wpiot-plugin.php
@@ -16,8 +16,8 @@ function wpiot_send_iot_message_on_comment_post($comment_id, $comment_approved, 
     
     try {
 
-      ///$connectionString = 'HostName=wpiothub.azure-devices.net;DeviceId=wpiotsite;SharedAccessKey=SdEzHCKOGLTnVvur7gs0VAgC7wsVsYjzWNlie3Urtg0=';
-      ///$client = new AzureIoTHub\DeviceClient($connectionString);
+      //$connectionString = 'HostName=wpiothub.azure-devices.net;DeviceId=wpiotsite;SharedAccessKey=SdEzHCKOGLTnVvur7gs0VAgC7wsVsYjzWNlie3Urtg0=';
+      //$client = new AzureIoTHub\DeviceClient($connectionString);
       //$response = $client->sendEvent('Comment Posted!' . $comment_data);
 
       //Try posting to Azure Function using Guzzle
